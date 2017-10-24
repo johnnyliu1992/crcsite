545c5b949b0c8d2136e649a89c07f5a061fdc3cd
Johnny Liu <liujiannanself@gmail.com>
2017-10-24 13:51:00 -0400

    oct2401

--- crcsite/admin.py
+++ crcsite/admin.py
@@ -4,3 +4,21 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question, Answer
+
+class AnswerInline(admin.StackedInline):
+    model = Answer
+    extra = 3
+    list_display=('A_text')
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['Q_text']}),
+        ('Question information', {'fields': ['Q_category', 'Q_type']}),
+    ]
+    list_display=('Q_text','Q_category', 'Q_type')
+    inlines = [AnswerInline]
+
+admin.site.register(Question, QuestionAdmin)
+
--- crcsite/models.py
+++ crcsite/models.py
@@ -3,4 +3,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    Q_ID=models.AutoField(primary_key=True)
+    Q_type=models.CharField(null=False, max_length=400)
+    Q_category=models.CharField(null=False, max_length=400)
+    Q_text=models.CharField(null=True, max_length=400)
+    def __str__(self):
+        return '%d %s %s %s' % (self.Q_ID, self.Q_type, self.Q_category, self.Q_text)
+        
+class Answer(models.Model):
+    A_ID=models.AutoField(primary_key=True)
+    Q_ID=models.ForeignKey(Question, on_delete=models.CASCADE)
+    A_text=models.CharField(null=True, max_length=400)
+    def __str__(self):
+        return '%d %s %s \n' % (self.A_ID, self.Q_ID, self.A_text)
